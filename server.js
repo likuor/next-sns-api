8fa105d429b8d43650a48e35564f9bc32d2a16d7
likuor <likuor0528@gmail.com>
2023-12-25 13:36:51 -0800

    add: api auth login

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const { PrismaClient } = require('@prisma/client');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config;
 
 const PORT = 5005;
 const prisma = new PrismaClient();
@@ -22,6 +24,33 @@ app.post('/api/auth/register', async (req, res) => {
   });
 
   return res.json({ user });
+});
+
+// Login user
+app.post('/api/auth/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await prisma.user.findUnique({ where: { email } });
+
+  if (!user) {
+    return res.status(401).json({
+      error: 'The user is not existed',
+    });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    return res.status(401).json({
+      error: 'The password is wrong',
+    });
+  }
+
+  const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, {
+    expiresIn: '1d',
+  });
+
+  return res.json({ token });
 });
 
 app.listen(PORT, () => console.log(`Server is runnnig on Port ${PORT}`));
