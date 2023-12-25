bb458f91860f1d269875346af45d36626d1cbc34
likuor <likuor0528@gmail.com>
2023-12-25 13:52:15 -0800

    add: api refactor folder structure

--- server.js
+++ server.js
@@ -1,56 +1,14 @@
 const express = require('express');
 const app = express();
-const { PrismaClient } = require('@prisma/client');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const authRoute = require('./routers/auth');
+
 require('dotenv').config;
 
 const PORT = 5005;
-const prisma = new PrismaClient();
+
 app.use(express.json());
 
-// Register new user
-app.post('/api/auth/register', async (req, res) => {
-  const { username, email, password } = req.body;
-
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  const user = await prisma.user.create({
-    data: {
-      username,
-      email,
-      password: hashedPassword,
-    },
-  });
-
-  return res.json({ user });
-});
-
-// Login user
-app.post('/api/auth/login', async (req, res) => {
-  const { email, password } = req.body;
-
-  const user = await prisma.user.findUnique({ where: { email } });
-
-  if (!user) {
-    return res.status(401).json({
-      error: 'The user is not existed',
-    });
-  }
-
-  const isPasswordValid = await bcrypt.compare(password, user.password);
-
-  if (!isPasswordValid) {
-    return res.status(401).json({
-      error: 'The password is wrong',
-    });
-  }
-
-  const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, {
-    expiresIn: '1d',
-  });
-
-  return res.json({ token });
-});
+// auth
+app.use('/api/auth', authRoute);
 
 app.listen(PORT, () => console.log(`Server is runnnig on Port ${PORT}`));
