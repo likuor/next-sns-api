aae160e3a6d20084c16e84e150db58a365c0d0bf
likuor <likuor0528@gmail.com>
2023-12-27 13:35:10 -0800

    add: auth middleware

---
+++ middlewares/isAuthenticated.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+
+const isAuthenticated = (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({
+      message: 'Not authorized',
+    });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
+    if (error) {
+      return res.status(401).json({
+        message: 'Not authorized',
+      });
+    }
+
+    req.userId = decoded.id;
+
+    next();
+  });
+};
+
+module.exports = isAuthenticated;
--- routers/posts.js
+++ routers/posts.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const { PrismaClient } = require('@prisma/client');
+const isAuthenticated = require('../middlewares/isAuthenticated');
 
 const prisma = new PrismaClient();
 
 // Post a tweet
-router.post('/post', async (req, res) => {
+router.post('/post', isAuthenticated, async (req, res) => {
   const { content } = req.body;
 
   if (!content) {
@@ -17,7 +18,7 @@ router.post('/post', async (req, res) => {
     const newPost = await prisma.post.create({
       data: {
         content,
-        authorId: 1,
+        authorId: req.userId,
       },
       include: {
         author: true,
